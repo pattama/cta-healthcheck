bf4094aaaf2b2f212da74000ab5db68d09ff533c
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-07-28 11:22:28 +0200

    added main status aggregation

--- test/lib/index.test.js
+++ test/lib/index.test.js
@@ -1,28 +1,87 @@
 'use strict';
 
-const assert = require('assert');
+const assert = require('chai').assert;
 const supertest = require('supertest');
 const Lib = require('../../lib');
 const lib = new Lib();
 
 describe('server', () => {
-  it('should update status', () => {
-    const status = {
+  it('should update service one to red status', () => {
+    lib.update('one', {
+      status: 'red',
+      reason: 'service down',
+    });
+    assert.strictEqual(lib.healths.status, 'red');
+    assert.strictEqual(lib.healths.statuses.one.status, 'red');
+    assert.strictEqual(lib.healths.statuses.one.current.services.default.status, 'red');
+    assert.strictEqual(lib.healths.statuses.one.current.services.default.reason, 'service down');
+    assert.deepEqual(lib.healths.statuses.one.previous, {});
+  });
+  it('should update service one to green status', () => {
+    lib.update('one', {
       status: 'green',
-      reason: 'ok',
-      services: [],
-    };
-    lib.update('one', status);
-    assert.deepEqual(lib.healths, {one: status});
+    });
+    assert.strictEqual(lib.healths.status, 'green');
+    assert.strictEqual(lib.healths.statuses.one.status, 'green');
+    assert.strictEqual(lib.healths.statuses.one.current.services.default.status, 'green');
+    assert.strictEqual(lib.healths.statuses.one.current.services.default.reason, '');
+    assert.strictEqual(lib.healths.statuses.one.previous.services.default.status, 'red');
+    assert.strictEqual(lib.healths.statuses.one.previous.services.default.reason, 'service down');
+  });
+  it('should update service two/alpha to yellow status', () => {
+    lib.update('two', {
+      status: 'yellow',
+      reason: 'critic point reached',
+      serviceName: 'alpha',
+    });
+    assert.strictEqual(lib.healths.status, 'yellow');
+    assert.strictEqual(lib.healths.statuses.one.status, 'green');
+    assert.strictEqual(lib.healths.statuses.two.status, 'yellow');
+    assert.strictEqual(lib.healths.statuses.two.current.services.alpha.status, 'yellow');
+    assert.strictEqual(lib.healths.statuses.two.current.services.alpha.reason, 'critic point reached');
+    assert.deepEqual(lib.healths.statuses.two.previous, {});
+  });
+  it('should update service two/beta to red status', () => {
+    lib.update('two', {
+      status: 'red',
+      reason: 'service down',
+      serviceName: 'beta',
+    });
+    assert.strictEqual(lib.healths.status, 'red');
+    assert.strictEqual(lib.healths.statuses.one.status, 'green');
+    assert.strictEqual(lib.healths.statuses.two.status, 'red');
+    assert.strictEqual(lib.healths.statuses.two.current.services.alpha.status, 'yellow');
+    assert.strictEqual(lib.healths.statuses.two.current.services.beta.status, 'red');
+    assert.strictEqual(lib.healths.statuses.two.current.services.beta.reason, 'service down');
+  });
+  it('should update service two/beta to green status', () => {
+    lib.update('two', {
+      status: 'green',
+      serviceName: 'beta',
+    });
+    assert.strictEqual(lib.healths.status, 'yellow');
+    assert.strictEqual(lib.healths.statuses.one.status, 'green');
+    assert.strictEqual(lib.healths.statuses.two.status, 'yellow');
+  });
+  it('should update service two/alpha to green status', () => {
+    lib.update('two', {
+      status: 'green',
+      serviceName: 'alpha',
+    });
+    assert.strictEqual(lib.healths.status, 'green');
+    assert.strictEqual(lib.healths.statuses.one.status, 'green');
+    assert.strictEqual(lib.healths.statuses.two.status, 'green');
   });
   it('should provide health as a restapi', (done) => {
-    const request = supertest('http://localhost:8080');
+    const request = supertest('http://localhost:' + lib.port);
     request.get('/')
       .end((err, resp) => {
         if (err) {
           return done(err);
         }
-        assert.deepEqual(resp.body, lib.healths);
+        assert.deepEqual(resp.body, {
+          status: lib.healths.status,
+        });
         done();
       });
   });
