5582048eeab801b6efcf17fa18835564e17fd3f5
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-07-29 12:43:23 +0200

    improved code coverage

--- test/lib/index.test.js
+++ test/lib/index.test.js
@@ -1,88 +1,20 @@
 'use strict';
 
 const assert = require('chai').assert;
-const supertest = require('supertest');
 const Lib = require('../../lib');
-const lib = new Lib();
+const instances = {};
 
-describe('server', () => {
-  it('should update service one to red status', () => {
-    lib.update('one', {
-      status: 'red',
-      reason: 'service down',
-    });
-    assert.strictEqual(lib.healths.status, 'red');
-    assert.strictEqual(lib.healths.statuses.one.status, 'red');
-    assert.strictEqual(lib.healths.statuses.one.current.services.default.status, 'red');
-    assert.strictEqual(lib.healths.statuses.one.current.services.default.reason, 'service down');
-    assert.deepEqual(lib.healths.statuses.one.previous, {});
+describe('singleton', () => {
+  it('should return same instance', () => {
+    instances.one = new Lib();
+    instances.one.foo = 'bar';
+    instances.two = new Lib();
+    assert.equal(instances.two.foo, 'bar');
   });
-  it('should update service one to green status', () => {
-    lib.update('one', {
-      status: 'green',
-    });
-    assert.strictEqual(lib.healths.status, 'green');
-    assert.strictEqual(lib.healths.statuses.one.status, 'green');
-    assert.strictEqual(lib.healths.statuses.one.current.services.default.status, 'green');
-    assert.notProperty(lib.healths.statuses.one.current.services.default, 'reason');
-    assert.strictEqual(lib.healths.statuses.one.previous.services.default.status, 'red');
-    assert.strictEqual(lib.healths.statuses.one.previous.services.default.reason, 'service down');
-  });
-  it('should update service two/alpha to yellow status', () => {
-    lib.update('two', {
-      status: 'yellow',
-      reason: 'critic point reached',
-      serviceName: 'alpha',
-    });
-    assert.strictEqual(lib.healths.status, 'yellow');
-    assert.strictEqual(lib.healths.statuses.one.status, 'green');
-    assert.strictEqual(lib.healths.statuses.two.status, 'yellow');
-    assert.strictEqual(lib.healths.statuses.two.current.services.alpha.status, 'yellow');
-    assert.strictEqual(lib.healths.statuses.two.current.services.alpha.reason, 'critic point reached');
-    assert.deepEqual(lib.healths.statuses.two.previous, {});
-  });
-  it('should update service two/beta to red status', () => {
-    lib.update('two', {
-      status: 'red',
-      reason: 'service down',
-      serviceName: 'beta',
-    });
-    assert.strictEqual(lib.healths.status, 'red');
-    assert.strictEqual(lib.healths.statuses.one.status, 'green');
-    assert.strictEqual(lib.healths.statuses.two.status, 'red');
-    assert.strictEqual(lib.healths.statuses.two.current.services.alpha.status, 'yellow');
-    assert.strictEqual(lib.healths.statuses.two.current.services.beta.status, 'red');
-    assert.strictEqual(lib.healths.statuses.two.current.services.beta.reason, 'service down');
-  });
-  it('should update service two/beta to green status', () => {
-    lib.update('two', {
-      status: 'green',
-      serviceName: 'beta',
-    });
-    assert.strictEqual(lib.healths.status, 'yellow');
-    assert.strictEqual(lib.healths.statuses.one.status, 'green');
-    assert.strictEqual(lib.healths.statuses.two.status, 'yellow');
-  });
-  it('should update service two/alpha to green status', () => {
-    lib.update('two', {
-      status: 'green',
-      serviceName: 'alpha',
-    });
-    assert.strictEqual(lib.healths.status, 'green');
-    assert.strictEqual(lib.healths.statuses.one.status, 'green');
-    assert.strictEqual(lib.healths.statuses.two.status, 'green');
-  });
-  it('should provide health as a restapi', (done) => {
-    const request = supertest('http://localhost:' + lib.port);
-    request.get('/')
-      .end((err, resp) => {
-        if (err) {
-          return done(err);
-        }
-        assert.deepEqual(resp.body, {
-          status: lib.healths.status,
-        });
-        done();
-      });
+  it('should return different instance', () => {
+    instances.three = new Lib({newInstance: true});
+    instances.four = new Lib({port: 3100});
+    assert.notProperty(instances.three, 'foo');
+    assert.notProperty(instances.four, 'foo');
   });
 });
