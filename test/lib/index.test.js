f34a404bb9da2bbf447220fdfe9015965a678177
Song An Bui <songan.bui@thomsonreuters.com>
2016-08-11 13:14:54 +0200

    replaces nodejs express dependency with cta-expresswrapper Instead of creating a new REST API with nodejs 'express' module, uses instead a cta-expresswrapper injected in the dependencies of the Healthcheck tool by Cement.

--- test/lib/index.test.js
+++ test/lib/index.test.js
@@ -2,28 +2,51 @@
 
 const assert = require('chai').assert;
 const supertest = require('supertest');
+const _ = require('lodash');
+
+const Express = require('cta-expresswrapper');
+const expressDependencies = {};
+const expressConfig = {
+  name: 'express',
+  properties: {
+    port: 8080,
+  },
+};
+const express = new Express(expressDependencies, expressConfig);
+
 const Lib = require('../../lib');
 const config = {
   name: 'healthCheck',
   properties: {},
-  singleton: true,
+  singleton: false,
 };
-const dependencies = {};
+const dependencies = {
+  express: express,
+};
 const healthCheck = new Lib(dependencies, config);
 
 describe('tests', () => {
   it('should set default params', () => {
-    assert.equal(healthCheck.port, 8080);
     assert.equal(healthCheck.queue, 'healthcheck');
   });
   it('should set custom params', () => {
-    dependencies.messaging = require('cta-messaging')();
-    config.properties.port = 3000;
+    // mock dependencies
+    const mockDependencies = _.cloneDeep(dependencies);
+    // mock messaging
+    mockDependencies.messaging = require('cta-messaging')();
+    // mock express on a custom port
+    const mockExpressConfig = _.cloneDeep(expressConfig);
+    mockExpressConfig.name = 'mockExpress';
+    mockExpressConfig.properties.port = 3000;
+    mockDependencies.express = new Express(expressDependencies, mockExpressConfig);
+
+    // mock config
     config.properties.queue = 'my_queue';
-    const myHealthCheck = new Lib(dependencies, config);
-    assert.equal(myHealthCheck.port, 3000);
+
+    const myHealthCheck = new Lib(mockDependencies, config);
     assert.equal(myHealthCheck.queue, 'my_queue');
     assert(myHealthCheck.messaging);
+    assert(myHealthCheck.express, mockDependencies.express);
   });
   it('should not send update when missing parameters', () => {
     healthCheck.update('one');
@@ -99,7 +122,7 @@ describe('tests', () => {
     assert.strictEqual(healthCheck.healths.statuses.two.status, 'green');
   });
   it('should provide / restapi', (done) => {
-    const request = supertest('http://localhost:' + healthCheck.port);
+    const request = supertest('http://localhost:' + healthCheck.express.port);
     request.get('/')
       .end((err, resp) => {
         if (err) {
@@ -112,7 +135,7 @@ describe('tests', () => {
       });
   });
   it('should provide /health restapi', (done) => {
-    const request = supertest('http://localhost:' + healthCheck.port);
+    const request = supertest('http://localhost:' + healthCheck.express.port);
     request.get('/health')
       .end((err, resp) => {
         if (err) {
@@ -125,18 +148,40 @@ describe('tests', () => {
   it('should return same instance without port conflict', (done) => {
     try {
       const instances = {};
-      config.properties.port = 3100;
-      config.singleton = true;
-      instances.one = new Lib(dependencies, config);
+      // mock express on a custom port
+      const mockDependencies = _.cloneDeep(dependencies);
+      const mockExpressConfig = _.cloneDeep(expressConfig);
+      mockExpressConfig.name = 'mockExpress';
+      mockExpressConfig.properties.port = 3100;
+      mockDependencies.express = new Express(expressDependencies, mockExpressConfig);
+
+      const mockConfig = _.cloneDeep(config);
+      mockConfig.singleton = true;
+      mockConfig.properties.queue = 'singleton_queue';
+      instances.one = new Lib(mockDependencies, mockConfig);
       setTimeout(() => {
-        instances.two = new Lib(dependencies, config);
+        instances.two = new Lib(mockDependencies, mockConfig);
         setTimeout(() => {
-          instances.three = new Lib(dependencies, config);
+          instances.three = new Lib(mockDependencies, mockConfig);
           done();
         }, 1000);
       }, 1000);
     } catch (e) {
       assert.fail(e, 'should not be here');
     }
+  });
+});
+
+describe('Healthcheck - constructor', function() {
+  context('when missing express dependency', function() {
+    const mockDependencies = _.cloneDeep(dependencies);
+    before(function() {
+      delete mockDependencies.express;
+    });
+    it('should throw an error', function() {
+      return assert.throws(function() {
+        return new Lib(mockDependencies, config);
+      }, `'express' dependency is missing`);
+    });
   });
 });
